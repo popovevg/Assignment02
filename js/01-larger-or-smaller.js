4171f2598312343d234adb613fb84d7204298509
popovevg <popovevg2013@gmail.com>
2020-02-11 17:58:15 -0800

    new

--- js/01-larger-or-smaller.js
+++ js/01-larger-or-smaller.js
@@ -1,13 +1,13 @@
-// let x = prompt("Enter first digit.");
-// x = Number(x);
-// let y = prompt("Enter second digit.");
-// y = Number(y);
-// if (x > y) {
-//     alert("The biggest diget is " + x);
-// }
-// else if (x == y) {
-//     alert("Digits are equal.");
-// }
-// else {
-//     alert("The biggest diget is " + y)
-// }
+let x = prompt("Enter first digit.");
+x = Number(x);
+let y = prompt("Enter second digit.");
+y = Number(y);
+if (x > y) {
+    alert("The biggest diget is " + x);
+}
+else if (x == y) {
+    alert("Digits are equal.");
+}
+else {
+    alert("The biggest diget is " + y)
+}
